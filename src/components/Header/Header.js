d3c3061167af5c2f7d92983031e6defc8d303692
Rahul H Sreedhar <rahul.h.1993@gmail.com>
2020-10-23 00:53:16 +0530

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import DehazeIcon from '@material-ui/icons/Dehaze';
+import CloseIcon from '@material-ui/icons/Close';
+import './Header.css'
+
+const Header = () => {
+
+  const baseURL = "";
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const appDrawerClick = () => {
+    setOpenDrawer(!openDrawer)
+  }
+
+
+  return (
+
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+
+          <Link to={`${baseURL}/home`}
+            className="navbar-logo"
+
+          >
+            <MenuBookIcon fontSize="large"
+              onClick={appDrawerClick}
+              style={{ fill: "grey" }}></MenuBookIcon>
+
+          </Link>
+
+
+
+          <div className="menu-icon"
+            onClick={appDrawerClick}>
+
+
+            ${openDrawer ? <CloseIcon style={{ color: "grey" }}></CloseIcon> :
+              <DehazeIcon style={{ color: "grey" }}></DehazeIcon>}
+
+          </div>
+
+          <ul className={openDrawer ? 'nav-menu active' : 'nav-menu'}>
+            <li>
+              <Link to={`${baseURL}/home`}>Home
+                </Link>
+            </li>
+            <li>
+              <Link to={`${baseURL}/home`}>Contact us</Link>
+            </li>
+            <li>
+              <Link to={`${baseURL}/home`}>About us</Link>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+
+
+    </>
+
+  )
+}
+export default Header;
