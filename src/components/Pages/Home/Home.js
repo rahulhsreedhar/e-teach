0b59095c6512b5d776f4a50b396ecb8ff69e37ef
Rahul H Sreedhar <rahul.h.1993@gmail.com>
2020-10-24 00:55:38 +0530

    add firebase and style change

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,22 +1,25 @@
 import React from "react";
-import Grid from '@material-ui/core/Grid';
 import './Home.css'
 const Home = () => {
     return (
         <div className="home">
-            <Grid container>
-                <Grid item xs={12} sm={6} className="main-content">
-                    <div className="main-text-wrapper">
-                        <div className="live-text">Online 1-on-1 LIVE Tuition</div>
-                        <div className="live-desc">Need-based one-on-one live online
-                        tuition classes for all subjects
+
+            <section className="intro">
+                <div className="intro_left">
+                    <div className="site-name">AIA Studies- Aim to Include All</div>
+                    <div className="live-text">Online 1-on-1 LIVE Tuition</div>
+                    <div className="live-desc">Need-based one-on-one live online
+                    tuition classes for all subjects
                     from experienced online tutors.</div>
-                    </div>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <img src="https://sportworldschool.com/wp-content/uploads/2020/09/teaching-support.jpg" width="500px" height="350px" />
-                </Grid>
-            </Grid>
+                </div>
+                <div className="intro_right">
+                    <img alt="image" src="https://sportworldschool.com/wp-content/uploads/2020/09/teaching-support.jpg" width="500px" height="350px" />
+                </div>
+
+
+            </section>
+
+
         </div>
     )
 }
